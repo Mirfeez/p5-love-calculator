a78d4f1a803301ceb654f36440b2da248e0074b9
Mirfeez <mahammadmirfeez@gmail.com>
2026-02-17 17:12:17 +0530

    oj

--- models/Love.js
+++ models/Love.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 const loveSchema = new mongoose.Schema({
-	date: {
-		type: Date,
-		default: Date.now,
-	},
-	him: String,
-	her: String,
-	score: Number,
+	him: { type: String, required: true },
+	her: { type: String, required: true },
+	score: { type: Number, required: true },
+	date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Love", loveSchema);
